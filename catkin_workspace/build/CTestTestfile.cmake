8d6cbcf405ec2f7693855c55c8f8edc2dee428be
Pranava Swaroopa <pranavaswaroopaa2000@gmail.com>
2022-02-15 23:46:04 -0500

    Delete catkin_workspace/build directory

--- catkin_workspace/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pranava/gitHub/Aue8230Spring2022_Group7/catkin_workspace/src
-# Build directory: /home/pranava/gitHub/Aue8230Spring2022_Group7/catkin_workspace/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_workspace/build/googletest/CMakeFiles/gtest.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/pranava/gitHub/Aue8230Spring2022_Group7/catkin_workspace/build/gmock/lib/libgtest.a"
-)
