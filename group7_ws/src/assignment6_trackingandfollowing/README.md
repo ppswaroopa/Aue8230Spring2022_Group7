785e35c29fe83baa40a7e7b681feeb1e5e5521ba
Pranava Swaroopa <pranavaswaroopaa2000@gmail.com>
2022-03-24 11:48:10 -0400

    assignment6 2nd commit

--- group7_ws/src/assignment6_trackingandfollowing/README.md
+++ group7_ws/src/assignment6_trackingandfollowing/README.md
@@ -1,24 +1,24 @@
 ## Homework 6 submission
 
-Package Name: assignment6_trackingandfollowing
-ROS Version: Noetic
-Required Package: 
-	1. gazebo
-	2. apriltag (AprilTag3)
-	3. apriltag_ros (3.1.2)
+Package Name: assignment6_trackingandfollowing<br>
+ROS Version: Noetic<br>
+Required Packages: <br>
+	1. gazebo<br>
+	2. apriltag (AprilTag3)<br>
+	3. apriltag_ros (3.1.2)<br>
 
-Prerequesite:
+Prerequisite:
  - Clone https://github.com/AprilRobotics/apriltag and https://github.com/AprilRobotics/apriltag_ros into your workspace.
- - Run in your workspace
  ```
- - $ cmake_isolated
+ $ cd ~/catkin_workspace/
+ $ cmake_isolated
  ```
- - To use the the routine that uses apriltag package, make sure to run both of these:
+ - To use the the routine that uses apriltag package, always include the setup.bash from devel_isolated together with the standard one:
  ```
  $ source devel/setup.bash
  $ source devel_isolated/setup.bash
  ```
-This package contains one routine performed on the Turtlebot3 Burger in Gazebo Simulation and two routines implemented to perform in the real turtlebot3 burger.
+This package contains one routine performed on the Turtlebot3 Burger in Gazebo Simulation and two routines implemented to perform in the Real life.
 
 ### How to Run
 Clone package folder into the catkin_workspace/src
@@ -27,12 +27,14 @@ $ catkin_make
 $ roslaunch assignment6_trackingandfollowing turtlebot3_follow_line.launch # Replace the launch file name with any other
 ```
 ### Demo
-1. wall_follower.py: Run the turtlebot in Gazebo with constant forward velocity while maintaining equal distances from the wall. A PD controller is implemented to keep the robot at the center between the walls.
-![](/screenshot/wall_follower.png)
-2. wander.py: Run the turtlebot in Gazebo around the world provided while avoiding any obstacle
-![](/screenshot/wander.png)
-3. obstacle_avoidance_realworld.py: This routine is run with the real robot. This is an implementation of the wander.py adapted to run in the real world obstacles.
-![](/screenshot/real_world.png)
+1. follow_line_step_hsv.py: Run the turtlebot in Gazebo with constant forward velocity while following the line in the 'follow_line.world' file. A PD controller is implemented to follow the lane.
+![](/screenshot/turtlebot3_follow_line_map.png)
+2. follow_line_step_hsv_real.py: To run the 1st routine in the Real world
+![](/screenshot/turtlebot3_follow_line_realworld.png)
+3. april_tag.py: Tracking an april tag of family 'Tag36h11' id:0 and maintains a constant distance of 0.3m. Make sure to follow the 'Prerequisite' and also replace the files found in 'catkin_workspace/src/apriltag_ros/apriltag_ros/config' with the ones provided in the 'other_res' directory<br>
+![Screen Record](/screenshot/apriltag_ros.png)
+!["Real World"](/screenshot/apriltag_ros_real.png)
 
 ### Demo Videos
-Videos folder inside the package contains demo runs of all the scripts.
+Videos folder inside the package contains demo runs of all the scripts in simulation and real world.
+
--- group7_ws/src/assignment6_trackingandfollowing/src/follow_line_step_hsv_real.py
+++ group7_ws/src/assignment6_trackingandfollowing/src/follow_line_step_hsv_real.py
@@ -6,7 +6,6 @@ from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
 from move_robot import MoveTurtlebot3
-from time import time
 
 class LineFollower(object):
 
@@ -16,29 +15,28 @@ class LineFollower(object):
         self.moveTurtlebot3_object = MoveTurtlebot3()
 
     def camera_callback(self, data):
+        global crop_img
         # We select bgr8 because its the OpneCV encoding by default
         cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
 
         # We get image dimensions and crop the parts of the image we dont need
         height, width, channels = cv_image.shape
-        crop_img = cv_image[int((height/2)+170):int((height/2)+190)][1:int(width)]
-        #crop_img = cv_image[340:360][1:640]
+        crop_img = cv_image[int((height/2))+150:int((height/2))+170][1:int(width)]
 
         # Convert from RGB to HSV
         hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
-        #rospy.loginfo(hsv)
 
-        # Define the Yellow Colour in HSV
+        # Define the Red Color in HSV
 
         """
         To know which color to track in HSV use ColorZilla to get the color registered by the camera in BGR and convert to HSV. 
         """
 
         # Threshold the HSV image to get only yellow colors
-        lower_yellow = np.array([160,100,80])
-        upper_yellow = np.array([180,255,255])
-        mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-        rospy.loginfo(mask)
+        # https://redketchup.io/color-picker
+        lower_white = np.array([150, 80, 90])
+        upper_white = np.array([179, 200, 200])
+        mask = cv2.inRange(hsv, lower_white, upper_white)
 
         # Calculate centroid of the blob of binary image using ImageMoments
         m = cv2.moments(mask, False)
@@ -49,29 +47,20 @@ class LineFollower(object):
             cx, cy = height/2, width/2
         
         # Draw the centroid in the resultut image
-        # cv2.circle(img, center, radius, color[, thickness[, lineType[, shift]]]) 
         cv2.circle(mask,(int(cx), int(cy)), 10,(0,0,255),-1)
         cv2.imshow("Original", cv_image)
         cv2.imshow("MASK", mask)
         cv2.waitKey(1)
 
         #################################
-        ########   CONTROLLER    ########
+        ########    CONTROLLER   ########
         #################################
-        
-        global cxlast
-        # PD controller, works in gazebo to follow line relatively in centre
-        twist_object = Twist()
-        twist_object.linear.x = 0.22
 
-        p = max(-0.15, min(0.15, ((width/2) - cx)/4000))
-        d = max(-0.15, min(0.15, (cxlast-cx)/(time() - lasttime)*1.5*1e6))
-        #twist_object.angular.z = p+d
-        twist_object.angular.z = p*1.5+d*6
-        print(f"{p=}\t{d=}")
+        a_max = 0.15
+        twist_object = Twist()
+        twist_object.linear.x = 0.15     
+        twist_object.angular.z = max(-a_max, min(a_max, ((width/2) - cx)/800))
         
-        cxlast = cx
-        # end controller
         rospy.loginfo("ANGULAR VALUE SENT===>"+str(twist_object.angular.z))
 
         # Make it start turning
@@ -87,7 +76,6 @@ def main():
     rate = rospy.Rate(5)
     ctrl_c = False
     def shutdownhook():
-        # Works better than rospy.is_shutdown()
         line_follower_object.clean_up()
         rospy.loginfo("Shutdown time!")
         ctrl_c = True
@@ -96,6 +84,4 @@ def main():
         rate.sleep()
 
 if __name__ == '__main__':
-        lasttime = 0
-        cxlast = 0
         main()
